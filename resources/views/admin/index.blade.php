94a7db75e9ef3db4fdee3255e4ae54f4150fc35f
fedebacci <federicobacci1999@gmail.com>
2025-09-29 12:23:33 +0200

    created master layout for my views and basic graphic structure

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,17 +1,31 @@
-<h1>
-    Admin dashboard
-</h1>
-<ul>
-    <li>
-        Nome: {{ $user['name'] }}
-    </li>
-    <li>
-        Email: {{ $user['email'] }}
-    </li>
-    <li>
-        Creato: {{ $user['created_at'] }}
-    </li>
-    <li>
-        Modificato: {{ $user['updated_at'] }}
-    </li>
-</ul>
+@extends('layouts.master')
+
+@section('title', 'Admin Dashboard')
+
+@section('content')
+    <section class="py-3">
+        <div class="container">
+            <div class="cor">
+                <div class="col-12">
+                    <h1>
+                        Admin dashboard
+                    </h1>
+                    <ul>
+                        <li>
+                            Nome: {{ $user['name'] }}
+                        </li>
+                        <li>
+                            Email: {{ $user['email'] }}
+                        </li>
+                        <li>
+                            Creato: {{ $user['created_at'] }}
+                        </li>
+                        <li>
+                            Modificato: {{ $user['updated_at'] }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -1,17 +1,31 @@
-<h1>
-    Admin profile
-</h1>
-<ul>
-    <li>
-        Nome: {{ $user['name'] }}
-    </li>
-    <li>
-        Email: {{ $user['email'] }}
-    </li>
-    <li>
-        Creato: {{ $user['created_at'] }}
-    </li>
-    <li>
-        Modificato: {{ $user['updated_at'] }}
-    </li>
-</ul>
+@extends('layouts.master')
+
+@section('title', 'Admin Profile')
+
+@section('content')
+<section class="py-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>
+                    Admin profile
+                </h1>
+                <ul>
+                    <li>
+                        Nome: {{ $user['name'] }}
+                    </li>
+                    <li>
+                        Email: {{ $user['email'] }}
+                    </li>
+                    <li>
+                        Creato: {{ $user['created_at'] }}
+                    </li>
+                    <li>
+                        Modificato: {{ $user['updated_at'] }}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
