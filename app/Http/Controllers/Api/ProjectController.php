387f77ee8ffcd3076efde77d617e44891434a02b
fedebacci <federicobacci1999@gmail.com>
2025-10-13 10:07:01 +0200

    correct retrieving of type and technologies for api/projects route

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,19 +13,7 @@ class ProjectController extends Controller
         // return "Sei nella index delle api di progetti";
 
         
-        $projects = Project::all();
-
-        // - Cerco di mappare le technologies prima di fornirle alla risposta, ma non funziona perchè è una cllection e non un array
-        // foreach($projects as $project) {
-        //     $project->technologies = array_map(function($technology) {
-        //         return ['name' => $technology['name'], 'color' => $technology['color']];
-        //     }, $project->technologies);
-        // }
-
-        foreach($projects as $project) {
-            // $project->technologies = json_decode($project->technologies);
-            $project->technologies = $project->technologies;
-        }
+        $projects = Project::with('type', 'technologies')->get();
 
         return response()->json([
             'success' => true,
